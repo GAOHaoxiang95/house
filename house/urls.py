e8caaee687501afb6265575606ecadefdde42e64
GAOHaoxiang95 <40728832+GAOHaoxiang95@users.noreply.github.com>
2019-05-12 16:48:45 +0800

    update

--- house/urls.py
+++ house/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from pricesite.views import homepage
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', homepage),
+    path('property/', homepage)
 ]
