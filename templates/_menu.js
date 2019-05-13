c17a9e3c717472dacb5ab92350c06479863f4e24
GAOHaoxiang95 <40728832+GAOHaoxiang95@users.noreply.github.com>
2019-05-13 13:24:12 +0800

    update2

---
+++ templates/_menu.js
@@ -0,0 +1,22 @@
+function setMenu(id)
+{
+    var select = document.getElementById(id);
+    for(var i=0;i<=10;i++){
+    var opt = document.createElement("option");
+    opt.value=i;
+    opt.innerHTML=i;
+    select.appendChild(opt);
+    }
+}
+
+function setProperty()
+{
+    var select = document.getElementById('property_type');
+    var items = ['Flat', 'Studio', 'House']
+    for(var i=0;i<items.length;i++){
+        var opt = document.createElement("option");
+        opt.value=i;
+        opt.innerHTML=items[i];
+        select.appendChild(opt);
+    }
+}
