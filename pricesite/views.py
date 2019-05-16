a20000f246bbd1ecc38fc7a19a937bb42e711b82
GAOHaoxiang95 <40728832+GAOHaoxiang95@users.noreply.github.com>
2019-05-16 21:42:56 +0800

    up

--- pricesite/views.py
+++ pricesite/views.py
@@ -5,4 +5,16 @@ from django.http import HttpResponse, Http404
 
 
 def homepage(request):
+    try:
+        num_beds = request.GET['num_beds']
+        num_baths = request.GET['num_baths']
+        num_recepts = request.GET['num_recepts']
+        postcode = request.GET['postcode']
+    except:
+        num_beds = 0
+        num_baths = 0
+        num_recepts = 0
+        postcode = None
+
+    price = 90 + int(num_beds) + int(num_baths) + int(num_recepts)
     return render(request, 'main.html', locals())
