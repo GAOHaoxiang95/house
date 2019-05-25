22fa06525fa8ab559cf0510ca4db957e079cbfd3
GAOHaoxiang95 <40728832+GAOHaoxiang95@users.noreply.github.com>
2019-05-25 21:35:00 +0800

    up

--- house/urls.py
+++ house/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.contrib import staticfiles
 from django.contrib import admin
 from django.urls import path
 from pricesite.views import homepage, property, result
@@ -22,3 +24,4 @@ urlpatterns = [
     path('property/', property),
     path('result/', result)
 ]
+urlpatterns += staticfiles_urlpatterns()
