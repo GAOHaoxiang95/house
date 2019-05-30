04be7559ab80e4e5e627ae640735b4c1bd12be4e
GAOHaoxiang95 <40728832+GAOHaoxiang95@users.noreply.github.com>
2019-05-30 22:26:38 +0800

    up

--- pricesite/models.py
+++ pricesite/models.py
@@ -7,6 +7,6 @@ class User(models.Model):
     name = models.CharField(max_length=20, null=False)
     password = models.CharField(max_length=20, null=False)
     email = models.EmailField()
-    
+
     def __str__(self):
         return self.name
