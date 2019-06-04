ea4a6df78bd63e91ed3021ce6c906e442267f446
GAOHaoxiang95 <40728832+GAOHaoxiang95@users.noreply.github.com>
2019-06-04 14:17:24 +0800

    up

--- pricesite/admin.py
+++ pricesite/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from pricesite import models
 
+admin.site.register(models.Preference)
+admin.site.register(models.User)
 # Register your models here.
--- pricesite/models.py
+++ pricesite/models.py
@@ -3,10 +3,23 @@ from django.db import models
 # Create your models here.
 
 
+class Preference(models.Model):
+    price = models.PositiveIntegerField(default=0)
+    latitude = models.DecimalField(default=50.0, max_digits=11, decimal_places=8)
+    longitude = models.DecimalField(default=0.0, max_digits=11, decimal_places=8)
+    beds = models.PositiveIntegerField(default=1)
+    baths = models.PositiveIntegerField(default=0)
+    school = models.DecimalField(default=0.0, max_digits=11, decimal_places=8)
+    transport = models.DecimalField(default=0.0, max_digits=11, decimal_places=8)
+    property_type = models.PositiveIntegerField(default=0)
+    furniture_state = models.PositiveIntegerField(default=0)
+
+
 class User(models.Model):
     name = models.CharField(max_length=20, null=False)
     password = models.CharField(max_length=20, null=False)
     email = models.EmailField()
+    prefer = models.ForeignKey(Preference, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
