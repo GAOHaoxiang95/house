3e20f3db349dc2f5a7154f8535060bd1f53a5c1d
GAOHaoxiang95 <40728832+GAOHaoxiang95@users.noreply.github.com>
2019-07-04 10:24:32 +0100

    marks

---
+++ pricesite/templates/menu.js
@@ -0,0 +1,34 @@
+function setMenu(id)
+{
+    var select = document.getElementById(id);
+    for(var i=0;i<=10;i++){
+    var opt = document.createElement("option");
+    opt.value=i;
+    opt.innerHTML=i;
+    select.appendChild(opt);
+    }
+}
+
+function setProperty()
+{
+    var select = document.getElementById('property_type');
+    var items = ['Detached House', 'Semi-detached House', 'Terraced House', 'Townhouse', 'Bungalow', 'Studio', 'Flat', 'Maisonette']
+    for(var i=0;i<items.length;i++){
+        var opt = document.createElement("option");
+        opt.value=i;
+        opt.innerHTML=items[i];
+        select.appendChild(opt);
+    }
+}
+
+function setFurniture()
+{
+    var select = document.getElementById('furniture_state');
+    var items = ['Unfurnished', 'Part furnished', 'Furnished']
+    for(var i=0;i<items.length;i++){
+        var opt = document.createElement("option");
+        opt.value=i;
+        opt.innerHTML=items[i];
+        select.appendChild(opt);
+    }
+}
