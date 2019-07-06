99a06b5614b0b1ae79657957c2f8d36d117aee4a
GAOHaoxiang95 <40728832+GAOHaoxiang95@users.noreply.github.com>
2019-07-06 16:09:54 +0100

    up

--- pricesite/recommendation.py
+++ pricesite/recommendation.py
@@ -13,9 +13,9 @@ class Recommendation(object):
         self.featureVector = np.zeros((num_of_items, num_of_features))
         furnished_state_dict = {'unfurnished': 0.0, 'furnished_or_unfurnished': 1.0, 'part_furnished': 2.0,
                                 'furnished': 3.0}
-        property_type_dict = {'detached': 0.0, 'semi_detached': 1.0, 'terraced': 2.0, 'end_terrace': 2.0,
-                              'town_house': 3.0, 'bungalow': 4.0, 'detached_bungalow': 4.0,
-                              'semi_detached_bungalow': 4.0, 'studio': 5.0, 'flat': 6.0, 'maisonette': 7.0}
+        property_type_dict = {'detached': 0.0, 'semi_detached': 0.0, 'terraced': 0.0, 'end_terrace': 0.0,
+                              'town_house': 1.0, 'bungalow': 2.0, 'detached_bungalow': 2.0,
+                              'semi_detached_bungalow': 2.0, 'studio': 3.0, 'flat': 4.0, 'maisonette': 4.0}
         for i in range(num_of_items):
             price = preference[i].price
             latitude = preference[i].latitude
