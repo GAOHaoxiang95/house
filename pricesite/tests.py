9151517b316c30950e8f86f737e98098216a09fb
GAOHaoxiang95 <40728832+GAOHaoxiang95@users.noreply.github.com>
2019-07-12 15:03:01 +0100

    Update tests.py

--- pricesite/tests.py
+++ pricesite/tests.py
@@ -2,13 +2,15 @@ from django.test import TestCase
 
 import requests
 
+'''
 r = requests.get('http://127.0.0.1:8000/api/prefer/')
 print(r.text)
+'''
 # Create your tests here.
 
 from collections import deque
-
-
+import sklearn
+import numpy as np
 class Stack:
     def __init__(self):
         self.items = deque()
@@ -18,3 +20,10 @@ class Stack:
         return self.items.pop()
     def empty(self):
         return len(self.items)==0
+import math
+x = np.array([0.000001,0.00002,30])
+y = np.array([10000,0.002,4000])
+a = x.dot(y)
+
+b= math.sqrt(sum(x**2))*math.sqrt(sum(y**2))
+print(a/b)
