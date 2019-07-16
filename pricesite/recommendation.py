02c3aca25a943ed19ae74c74e969d4951171bbe8
GAOHaoxiang95 <40728832+GAOHaoxiang95@users.noreply.github.com>
2019-07-16 19:25:13 +0100

    Update recommendation.py

--- pricesite/recommendation.py
+++ pricesite/recommendation.py
@@ -83,7 +83,7 @@ class ReccomendationContentBased:
                               'semi_detached_bungalow': 1.0,
                               'studio': 2.0, 'flat': 3.0, 'maisonette': 3.0}
         preference = models.Profile.objects.get(user=user).prefer
-        x = [preference.price, preference.latitude*10000, preference.longitude*10000, preference.baths, preference.furniture_state, preference.property_type]
+        x = [preference.price, preference.latitude*100000, preference.longitude*100000, preference.baths, preference.furniture_state, preference.property_type]
         x = np.array(list(map_float(x)))
         self.settings = x
 
@@ -99,13 +99,13 @@ class ReccomendationContentBased:
                 pt = 1.5
             else:
                 pt = property_type_dict[i.property_type]
-            y = np.array([i.price_actual, i.latitude*10000, i.longitude*10000, i.num_baths, fs, pt])
+            y = np.array([i.price_actual, i.latitude*100000, i.longitude*100000, i.num_baths, fs, pt])
 
             a = x.dot(y)
             b = math.sqrt(sum(x ** 2)) * math.sqrt(sum(y ** 2))
             score = a/b#cosine similarity
-            y[1] = y[1]/10000
-            y[2] = y[2]/10000
+            y[1] = y[1]/100000
+            y[2] = y[2]/100000
             if i.num_beds >= preference.beds:
                 item = Item(y, score, i.URL, i.num_beds)
                 self.items.append(item)
