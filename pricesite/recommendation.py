16c61e38ac0f8879b7a1c6a9bdf7382697b079a7
GAOHaoxiang95 <40728832+GAOHaoxiang95@users.noreply.github.com>
2019-08-01 19:34:11 +0100

    u

--- pricesite/recommendation.py
+++ pricesite/recommendation.py
@@ -111,7 +111,7 @@ class ReccomendationContentBased:
         x = np.array(list(map_float(x)))
         self.settings = x
 
-        houses = models.House.objects.all()[0:3000]
+        houses = models.House.objects.all().order_by('-id')[0:3000]
         self.items = list()
         for i in houses:#论文可以吹 这么处理数据！！！！！！！！！！！！！！！！！！！！！
             if i.furnished_state == "":
