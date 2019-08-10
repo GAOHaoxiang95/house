5d837acbff9d1eff12212bd10dfe2863acb648c2
GAOHaoxiang95 <40728832+GAOHaoxiang95@users.noreply.github.com>
2019-08-10 20:54:46 +0100

    Update admin.py

--- pricesite/admin.py
+++ pricesite/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from pricesite import models
 
+
 admin.site.register(models.Preference)
 admin.site.register(models.User)
 admin.site.register(models.Profile)
 admin.site.register(models.PreferenceHouses)
+admin.site.register(models.Hot)
 # Register your models here.
